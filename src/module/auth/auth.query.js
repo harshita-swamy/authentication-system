7773d7f85317bb4efa2ba76a3522a59474f51f1a
Harshita Swamy <harshitaswamy.connect@gmail.com>
2026-08-04 19:21:07 +0530

    added LoginService

--- src/module/auth/auth.query.js
+++ src/module/auth/auth.query.js
@@ -1,3 +1,5 @@
 export const check_email = `SELECT * FROM users WHERE email = ?`;
 
 export const insert_user = `INSERT INTO users (name, email, password, phone) VALUES(?, ?, ?, ?)`;   
+
+export const getEmail_byID = 'SELECT * FROM users WHERE email = ?';
--- src/module/auth/auth.service.js
+++ src/module/auth/auth.service.js
@@ -2,7 +2,9 @@ import bcrypt from "bcrypt";
 import { check_email, insert_user } from "./auth.query.js";
 import db from "../../config/db.config.js";
 
-const registerService = async (userData) => {
+// ================================REGISTER======================================
+
+const registerService = async (userData) => {''
     const { name, email, password, phone } = userData;
 
     // Check Email
@@ -26,4 +28,48 @@ const registerService = async (userData) => {
     return result;
 };
 
-export { registerService };
+// ================================ LOGIN ===================================
+
+const loginService = async (mydata) =>{
+    const {email, password} = mydata;
+
+    const [existingData] = await db.execute(check_email, [email]);
+
+    if(existingData === 0){
+        throw new Error('Invalid Email')
+    }
+
+    const user = user[0];
+
+    const isMatch = bcrypt.compare(password, user.password);
+
+    if(!isMatch){
+        throw new Error('Invalid Password')
+    }
+
+    // Generate Token:
+
+    const token = jwt.sign(
+        {
+            id:user.id,
+            email:user.email
+        },
+        process.env.JWT_SECRET,
+
+        {
+            expiresIn: "1d"
+        }
+
+    );
+    return{
+        token,
+        user:{
+            id:user.id,
+            name:user.name,
+            email:user.email,
+            phone:user.phone
+        }
+    };
+};
+
+export { registerService, loginService };
