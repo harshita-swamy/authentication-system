73d1773a225acaf6b2b866393becbf4ae5185b97
Harshita Swamy <harshitaswamy.connect@gmail.com>
2026-08-05 21:11:42 +0530

    updated DB file

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -8,7 +8,10 @@ const pool = mysql.createPool({
     user:process.env.DB_USER,
     database:process.env.DB_NAME,
     password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT
+    port:process.env.DB_PORT,
+    waitForConnections:true,
+    connectionLimit:10,
+    queueLimit:0
 })
 
 export default pool;
